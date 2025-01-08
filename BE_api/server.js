4faef0ab3699d18bef28cc7187fbe92c2747011c
jaynguyen1510 <ngnhatnam1510@gmail.com>
2025-01-08 17:02:51 +0700

    change fixe

--- BE_api/server.js
+++ BE_api/server.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Lấy __dirname trong ES module
-const __dirname = new URL(".", import.meta.url).pathname;
+const __dirname = path.resolve();
 
 // Middleware
 app.use(cors());
@@ -18,15 +18,14 @@ app.use(express.json());
 // Dịch vụ API
 app.use("/api", apiRouter);
 
+app.use(express.static(path.join(__dirname, "/screenshot-cut/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "screenshot-cut", "dist", "index.html"));
+});
+
 // Middleware xử lý lỗi
 app.use(errorMiddleware);
-
-app.use(express.static(path.join(__dirname, "../screenshot-cut/build"))); // Cấu hình để phục vụ tệp từ build
-
-// Dịch vụ route cho các trang React (SPA)
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../screenshot-cut/build", "index.html"));
-});
 
 // Khởi động server
 app.listen(port, "0.0.0.0", () => {
